d641e737cfde6cfa462dd5e9bafecdf5957d7a5b
Zheng(Zeb) Yang <zhengzeb.yang@mail.utoronto.ca>
2024-04-01 19:50:42 -0400

    Download the data

--- scripts/01-download_data.R
+++ scripts/01-download_data.R
@@ -1,12 +1,8 @@
 #### Preamble ####
-# Purpose: Downloads and saves the data from [...UPDATE THIS...]
-# Author: Rohan Alexander [...UPDATE THIS...]
-# Date: 11 February 2023 [...UPDATE THIS...]
-# Contact: rohan.alexander@utoronto.ca [...UPDATE THIS...]
-# License: MIT
-# Pre-requisites: [...UPDATE THIS...]
-# Any other information needed? [...UPDATE THIS...]
-
+# Purpose: Downloads and saves the data from Basketabll-Reference
+# Authors: Zheng(Zeb) Yang
+# Contact: zhengzeb.yang@mail.utoronto.ca
+# Pre-requisites: -
 
 #### Workspace setup ####
 library(opendatatoronto)
@@ -14,13 +10,8 @@ library(tidyverse)
 # [...UPDATE THIS...]
 
 #### Download data ####
-# [...ADD CODE HERE TO DOWNLOAD...]
+# All data is downloaded from https://www.basketball-reference.com/leagues/
 
 
-
-#### Save data ####
-# [...UPDATE THIS...]
-# change the_raw_data to whatever name you assigned when you downloaded it.
-write_csv(the_raw_data, "inputs/data/raw_data.csv") 
 
          
