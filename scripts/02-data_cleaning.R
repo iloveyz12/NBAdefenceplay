106584819d4230ace998ade1d87c0c62f33049a0
Zheng(Zeb) Yang <zhengzeb.yang@mail.utoronto.ca>
2024-04-01 19:47:46 -0400

    Download and clean the data

--- scripts/02-data_cleaning.R
+++ scripts/02-data_cleaning.R
@@ -1,44 +1,33 @@
 #### Preamble ####
-# Purpose: Cleans the raw plane data recorded by two observers..... [...UPDATE THIS...]
-# Author: Rohan Alexander [...UPDATE THIS...]
-# Date: 6 April 2023 [...UPDATE THIS...]
-# Contact: rohan.alexander@utoronto.ca [...UPDATE THIS...]
-# License: MIT
-# Pre-requisites: [...UPDATE THIS...]
-# Any other information needed? [...UPDATE THIS...]
+# Purpose: Cleans the raw data 
+# Authors: Zheng(Zeb) Yang
+# Contact: zhengzeb.yang@mail.utoronto.ca
+# Pre-requisites: -
 
 #### Workspace setup ####
 library(tidyverse)
+library(readxl)
 
 #### Clean data ####
-raw_data <- read_csv("inputs/data/plane_data.csv")
+raw2023 <- read_excel("data/raw_data/2023general.xlsx")
+raw2022 <- read_excel("data/raw_data/2022general.xlsx")
+raw2021 <- read_excel("data/raw_data/2021general.xlsx")
+raw2020 <- read_excel("data/raw_data/2020general.xlsx")
 
-cleaned_data <-
-  raw_data |>
-  janitor::clean_names() |>
-  select(wing_width_mm, wing_length_mm, flying_time_sec_first_timer) |>
-  filter(wing_width_mm != "caw") |>
-  mutate(
-    flying_time_sec_first_timer = if_else(flying_time_sec_first_timer == "1,35",
-                                   "1.35",
-                                   flying_time_sec_first_timer)
-  ) |>
-  mutate(wing_width_mm = if_else(wing_width_mm == "490",
-                                 "49",
-                                 wing_width_mm)) |>
-  mutate(wing_width_mm = if_else(wing_width_mm == "6",
-                                 "60",
-                                 wing_width_mm)) |>
-  mutate(
-    wing_width_mm = as.numeric(wing_width_mm),
-    wing_length_mm = as.numeric(wing_length_mm),
-    flying_time_sec_first_timer = as.numeric(flying_time_sec_first_timer)
-  ) |>
-  rename(flying_time = flying_time_sec_first_timer,
-         width = wing_width_mm,
-         length = wing_length_mm
-         ) |> 
-  tidyr::drop_na()
+drbpt2023 <- raw2023 |>
+  select(DRB, PTS)
+
+drbpt2022 <- raw2022 |>
+  select(DRB, PTS)
+
+drbpt2021 <- raw2021 |>
+  select(DRB, PTS)
+
+drbpt2020 <- raw2020 |>
+  select(DRB, PTS)
+
+cleaned_four_seasons <- rbind(drbpt2023, drbpt2022) |> rbind(drbpt2021) |>rbind(drbpt2020) |>
+  select(DRB, PTS)
 
 #### Save data ####
-write_csv(cleaned_data, "outputs/data/analysis_data.csv")
+write_csv(cleaned_four_seasons, "data/analysis_data/analysis_data.csv")
