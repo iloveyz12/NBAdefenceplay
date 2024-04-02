ea1089b28db729d3b340da7064b206d80ee47535
Zheng(Zeb) Yang <zhengzeb.yang@mail.utoronto.ca>
2024-04-01 20:31:38 -0400

    Model

--- scripts/04-model.R
+++ scripts/04-model.R
@@ -1,11 +1,8 @@
 #### Preamble ####
-# Purpose: Models... [...UPDATE THIS...]
-# Author: Rohan Alexander [...UPDATE THIS...]
-# Date: 11 February 2023 [...UPDATE THIS...]
-# Contact: rohan.alexander@utoronto.ca [...UPDATE THIS...]
-# License: MIT
-# Pre-requisites: [...UPDATE THIS...]
-# Any other information needed? [...UPDATE THIS...]
+# Purpose: Models on analysis data
+# Authors: Zheng(Zeb) Yang
+# Contact: zhengzeb.yang@mail.utoronto.ca
+# Pre-requisites: -
 
 
 #### Workspace setup ####
@@ -17,16 +14,12 @@ analysis_data <- read_csv("data/analysis_data/analysis_data.csv")
 
 ### Model data ####
 first_model <-
-  stan_glm(
-    formula = flying_time ~ length + width,
-    data = analysis_data,
-    family = gaussian(),
-    prior = normal(location = 0, scale = 2.5, autoscale = TRUE),
-    prior_intercept = normal(location = 0, scale = 2.5, autoscale = TRUE),
-    prior_aux = exponential(rate = 1, autoscale = TRUE),
-    seed = 853
+  lm(
+    PTS ~ DRB,
+    data = drbpt4,
   )
 
+summary(first_model)
 
 #### Save model ####
 saveRDS(
