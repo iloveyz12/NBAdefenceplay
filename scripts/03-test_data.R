e0331cabf4899f6b5cb5cc1a7c38507da95e1d2d
Zheng(Zeb) Yang <zhengzeb.yang@mail.utoronto.ca>
2024-04-01 20:08:13 -0400

    Run the tests for analysis data

--- scripts/03-test_data.R
+++ scripts/03-test_data.R
@@ -1,15 +1,37 @@
 #### Preamble ####
-# Purpose: Tests... [...UPDATE THIS...]
-# Author: Rohan Alexander [...UPDATE THIS...]
-# Date: 11 February 2023 [...UPDATE THIS...]
-# Contact: rohan.alexander@utoronto.ca [...UPDATE THIS...]
-# License: MIT
-# Pre-requisites: [...UPDATE THIS...]
-# Any other information needed? [...UPDATE THIS...]
+# Purpose: Tests on real analysis data
+# Authors: Zheng(Zeb) Yang
+# Contact: zhengzeb.yang@mail.utoronto.ca
+# Pre-requisites: -
+
 
 
 #### Workspace setup ####
 library(tidyverse)
-# [...UPDATE THIS...]
+library(testthat)
 
-#### Test data ####
+#### Run the analysis data ####
+drbpt4 <- read.csv("data/analysis_data/analysis_data.csv")
+
+#### Test the data ####
+test_that("Check class", {
+  expect_type(drbpt4$DRB, "double")
+  expect_type(drbpt4$PTS, "double")
+  
+})
+
+
+
+test_that("Check complete", {
+  expect_true(all(complete.cases(drbpt4)))
+})
+
+
+test_that("Check variable",{
+  expect_true(drbpt4$DRB |> min() >= 0)
+  expect_true(drbpt4$DRB |> max() <= 100)
+  expect_true(drbpt4$PTS |> min() >= 0)
+  expect_true(drbpt4$PTS |> max() <= 200)
+})
+
+
