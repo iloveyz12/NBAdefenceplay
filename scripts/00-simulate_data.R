3da67fa03ba054f587bbd8e081879c7bc62cc137
Zheng(Zeb) Yang <zhengzeb.yang@mail.utoronto.ca>
2024-04-17 16:36:27 -0400

    Simulate data

--- scripts/00-simulate_data.R
+++ scripts/00-simulate_data.R
@@ -1,19 +1,22 @@
 #### Preamble ####
-# Purpose: Simulates... [...UPDATE THIS...]
-# Author: Rohan Alexander [...UPDATE THIS...]
-# Date: 11 February 2023 [...UPDATE THIS...]
-# Contact: rohan.alexander@utoronto.ca [...UPDATE THIS...]
-# License: MIT
-# Pre-requisites: [...UPDATE THIS...]
-# Any other information needed? [...UPDATE THIS...]
+# Purpose: Simulates data on 
+# Authors: Zheng(Zeb) Yang
+# Contact: zhengzeb.yang@mail.utoronto.ca
+# Pre-requisites: -
 
 
 #### Workspace setup ####
 library(tidyverse)
-# [...UPDATE THIS...]
+
 
 #### Simulate data ####
-# [...ADD CODE HERE...]
+set.seed(812)
 
+num_obs <- 120
 
+simDRB <- tibble(
+  DRB =round(runif(num_obs, min = 30, max = 35), 1),
+  PTS = round(rnorm(num_obs, 110, 5),1)
+) 
 
+write.csv(simDRB, "data/simulated_data/simulated_data.csv")
